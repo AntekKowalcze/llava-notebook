ec448da417f8f8542842e3328e9cb7ddb25c00f2
Antoni Kowalcze <antekkowalcze8@gmail.com>
2025-12-02 21:01:49 +0100

    added minor changes in configs + installed some packages

--- desktop/src/App.vue
+++ desktop/src/App.vue
@@ -29,8 +29,7 @@ async function greet() {
         
         <button
           @click="greet"
-          class="w-full px-6 py-3 bg-indigo-600 text-white font-semibold rounded-lg hover:bg-indigo-700 transition-colors duration-200"
-        >
+          class="w-full px-6 py-3 bg-indigo-600 text-white font-semibold rounded-lg hover:bg-indigo-700 transition-colors duration-200">
           Greet
         </button>
         
